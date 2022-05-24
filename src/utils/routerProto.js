754d03109a4c13033424aa60f58f56481c36e288
caoao <chou45169@163.com>
2022-05-24 12:26:20 +0800

    fix:修改权限问题

--- src/utils/routerProto.js
+++ src/utils/routerProto.js
@@ -1,7 +1,9 @@
 //路由拦截权限管理
 import router from '@/router'
 import store from '@/store'
+let handleRoutes = ''
 router.beforeEach(async (to, from, next) => {
+    console.log('to',to);
     // 1.判断是否登陆界面
     if (to.path == '/login' || to.path == '/bigScreen') {
         if (to.path == '/login') {
@@ -19,6 +21,10 @@ router.beforeEach(async (to, from, next) => {
         // 1.判断角色是否有（如果获取角色失败）
         var hasRoles = store && store.getters.roles && store.getters.roles.length > 0;
         if (hasRoles) {
+            console.log(router);
+            if(to.path == "/home/alarmData/intelligenceShow"){
+                next(handleRoutes)
+            }
             next();
         } else {
             //发送请求获取对应的角色信息
@@ -26,6 +32,13 @@ router.beforeEach(async (to, from, next) => {
                 var roles = await store.dispatch('GetInfo');
                 let rolesName = roles.roles;//得到角色
                 var accessRoutes = await store.dispatch('GenerateRoutes', rolesName);//角色过滤
+                let Alarmroutes = ''
+                if(accessRoutes[0].children.length>=3){
+                    Alarmroutes = accessRoutes[0].children[1].children[0].children
+                    if(Alarmroutes&&Alarmroutes.length>0){
+                        handleRoutes=  Alarmroutes[0]
+                    }
+                }
                 // router.addRoute(accessRoutes[0])
                 router.addRoutes(accessRoutes)
                 if (to.path == "/home") {//判断如果是home就跳转第一个页面
