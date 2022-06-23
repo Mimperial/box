02fdba233c6f908a0e88ff01f704da7a74ac48dc
zhanghongwei <zhanghongwei@objecteye.com>
2022-06-23 17:08:57 +0800

    人脸库

--- src/views/config/smart/event.vue
+++ src/views/config/smart/event.vue
@@ -409,14 +409,14 @@ export default {
             var data = JSON.parse(res.data);
             console.log(data, "data-----");
             if (data && data.length > 0) {
-              if (data[0].GroupIds && data[0].GroupIds.length > 1) {
-                this.peopleIds = data[0].GroupIds.split(",");
-              } else {
-                this.peopleIds = [];
-              }
               if (setSelectCamerId) {
                 //设置初始化相机选择
                 this.selectCamerId = data[0].id;
+                if (data[0].GroupIds && data[0].GroupIds.length > 1) {
+                  this.peopleIds = data[0].GroupIds.split(",");
+                } else {
+                  this.peopleIds = [];
+                }
               }
               this.selectChannels = data.map((item) => {
                 //这里进行测报警信息数据转换成json格式
